7ef4b15f3ab12344a533861ab36467dc55fa2636
Nicolas Payneau <nicolas.payneau@gmail.com>
2017-11-06 17:51:51 +0100

    Ajout de TU et modification du repo Employee

--- src/main/java/com/thales/codingdojo/employeeskills/service/EmployeeRepository.java
+++ src/main/java/com/thales/codingdojo/employeeskills/service/EmployeeRepository.java
@@ -4,5 +4,10 @@ package com.thales.codingdojo.employeeskills.service;
 import com.thales.codingdojo.employeeskills.domain.Employee;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+    List<Employee> findByLastNameOrderByFirstNameAsc(String lastname);
+    int countByLastNameIgnoreCase(String lastname);
 }
--- src/test/java/com/thales/codingdojo/employeeskills/EmployeeRepositoryTest.java
+++ src/test/java/com/thales/codingdojo/employeeskills/EmployeeRepositoryTest.java
@@ -1,21 +1,17 @@
 package com.thales.codingdojo.employeeskills;
 
-import com.thales.codingdojo.employeeskills.controller.EmployeesController;
 import com.thales.codingdojo.employeeskills.domain.Employee;
 import com.thales.codingdojo.employeeskills.domain.Skill;
 import com.thales.codingdojo.employeeskills.service.EmployeeRepository;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -23,31 +19,24 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.Date;
 import java.util.HashSet;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@WebAppConfiguration
-public class EmployeeControllerTest {
-
-    private MockMvc mockWebApp;
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
+public class EmployeeRepositoryTest {
 
     @Autowired
     private EmployeeRepository repo;
 
     @Before
     public void init() throws ParseException {
-        this.mockWebApp = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         repo.save(new Employee(null,"Payneau", "Nicolas", localDateToDate(LocalDate.of(1985, Month.DECEMBER, 12)), new HashSet<Skill>()));
+        repo.save(new Employee(null,"Payneau", "nicolas", localDateToDate(LocalDate.of(1985, Month.DECEMBER, 12)), new HashSet<Skill>()));
+        repo.save(new Employee(null,"Payneau", "Nico", localDateToDate(LocalDate.of(1985, Month.DECEMBER, 12)), new HashSet<Skill>()));
         repo.save(new Employee(null,"Guillet", "Matthieu", localDateToDate(LocalDate.of(1985, Month.APRIL, 12)), new HashSet<Skill>()));
-        repo.save(new Employee(null,"Florian", "Dupond", localDateToDate(LocalDate.of(1955, Month.AUGUST, 12)), new HashSet<Skill>()));
+        repo.save(new Employee(null,"Dupont", "Julien", localDateToDate(LocalDate.of(1955, Month.AUGUST, 12)), new HashSet<Skill>()));
+        repo.save(new Employee(null,"Dupont", "Nicolas", localDateToDate(LocalDate.of(1955, Month.AUGUST, 12)), new HashSet<Skill>()));
+
     }
 
     private Date localDateToDate(LocalDate ld) throws ParseException {
@@ -55,12 +44,14 @@ public class EmployeeControllerTest {
     }
 
     @Test
-    public void getEmployees() throws Exception {
-        mockWebApp.perform(get("/employees"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("$.[0].firstName", is("Payneau")));
+    public void findByLastnameOrderByFirstnameAsc() throws Exception {
+        List<Employee> result = repo.findByLastNameOrderByFirstNameAsc("Nicolas");
+        Assert.assertEquals("Dupont",result.get(0).getFirstName());
     }
 
 
+    @Test
+    public void countByLastnameIgnoreCase() throws Exception {
+        Assert.assertEquals(3,repo.countByLastNameIgnoreCase("Nicolas"));
+    }
 }
