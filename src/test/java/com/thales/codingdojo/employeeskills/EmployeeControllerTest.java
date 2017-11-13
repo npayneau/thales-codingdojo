ddc72e7b31c76569d39abe3253c6c4e3a25aec74
Mathieu Guillet <mathieu.guillet@thalesgroup.com>
2017-11-13 11:50:18 +0100

    Merge des modification pour les query methods

--- src/test/java/com/thales/codingdojo/employeeskills/EmployeeControllerTest.java
+++ src/test/java/com/thales/codingdojo/employeeskills/EmployeeControllerTest.java
@@ -14,7 +14,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.test.web.servlet.setup.ConfigurableMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcConfigurer;
+import org.springframework.test.web.servlet.setup.MockMvcConfigurerAdapter;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.text.ParseException;
