5b5e8aa28ac455d5a0d9e539fa3eb2e09a83c756
Mathieu Guillet <mathieu.guillet@thalesgroup.com>
2017-11-14 10:27:18 +0100

    Ajout d'un script pour peupler la base pour la recherche et pagination des Employees.

---
+++ populate.sh
@@ -0,0 +1,25 @@
+
+
+curl -d '{"firstName": "Robert","lastName": "Cook","birthDate": "28/02/1966"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Robin","lastName": "Soncrusoe","birthDate": "13/01/1968"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Jean-Claude","lastName": "Milou","birthDate": "21/04/1974"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Jean","lastName": "Cooker","birthDate": "28/02/1966"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Josette","lastName": "Cooking","birthDate": "14/02/1987"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Michele","lastName": "Girafe","birthDate": "27/07/1980"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Marie","lastName": "Chook","birthDate": "25/12/1959"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Roberto","lastName": "Cookoo","birthDate": "28/02/1986"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Scarlett","lastName": "Johansson","birthDate": "22/11/1984"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Jean-Guy","lastName": "Mook","birthDate": "28/02/1969"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Pascal","lastName": "Sevrant","birthDate": "12/03/1966"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Daniel","lastName": "Bruel","birthDate": "28/02/1967"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Patrick","lastName": "Balavoine","birthDate": "15/02/1975"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Chantal","lastName": "Baloseille","birthDate": "28/02/1979"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Myriam","lastName": "Moutin","birthDate": "20/09/1978"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Anatole","lastName": "Minorque","birthDate": "28/10/1979"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Annie","lastName": "Cordie","birthDate": "29/03/1989"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Jordie","lastName": "Gala","birthDate": "18/01/1990"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Jean-Luc","lastName": "Skywalker","birthDate": "28/07/1966"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Mireil","lastName": "Darkvador","birthDate": "06/08/1985"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Leyla","lastName": "Skyrunner","birthDate": "10/12/1984"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+curl -d '{"firstName": "Mickael","lastName": "Balliverne","birthDate": "30/06/1986"}' -H "Content-Type: application/json" -X POST http://localhost:1111/api/employees
+
